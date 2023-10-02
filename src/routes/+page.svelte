523e428b75a285258a58fb6bb4a1d21c2fb9cf25
Raunak Hajela <raunakhajela@gmail.com>
2023-10-02 14:46:25 +0000

    Add header and hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,186 @@
-<div class="flex items-center justify-center flex-col gap-5 p-5">
-    <h1 class="text-lg text-black">Welcome to SvelteKit with TailwindCSS</h1>
-        <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-</div>
+<header>
+	<nav
+		class="flex mx-auto max-w-screen-xl lg:max-w-screen-2xl items-center justify-between py-6 lg:px-3"
+		aria-label="Global"
+	>
+		<div class="flex lg:flex-1">
+			<a href="/" class="-m-1.5 p-1.5">
+				<img class="w-48" src="./logo-white.svg" alt="" />
+			</a>
+		</div>
+		<div class="flex lg:hidden">
+			<button
+				type="button"
+				class="mob-nav-trigger -m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+			>
+				<svg
+					class="h-6 w-6"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					aria-hidden="true"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+					/>
+				</svg>
+			</button>
+		</div>
+		<div class="hidden lg:flex lg:gap-x-12">
+			<a href="/" class="text-base leading-6 hover:underline">Home</a>
+			<a href="/" class="text-base leading-6 hover:underline">About</a>
+			<a href="/" class="text-base leading-6 hover:underline">Team</a>
+			<a href="/" class="text-base leading-6 hover:underline">Investment</a>
+		</div>
+		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
+			<a href="/" class="text-base leading-6 hover:font-semibold border border-[#0086e2] rounded-full px-8 py-2">Apply for funding <span aria-hidden="true">&rarr;</span></a
+			>
+		</div>
+	</nav>
+	<!-- Mobile menu starts here -->
+	<div class="navbar-mobile lg:hidden hidden" role="dialog">
+		<div class="fixed inset-0 z-50"></div>
+		<div
+			class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-[#ededed]/10"
+		>
+			<div class="flex items-center justify-between">
+				<a href="/" class=" ">
+					<img class="w-48" src="./logo-white.svg" alt="" />
+				</a>
+				<button type="button" class="mob-nav-close -m-2.5 rounded-md p-2.5 text-gray-700">
+					<svg
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
+			<div class="mt-6 flow-root">
+				<div class="-my-6 divide-y divide-[#ededed]0/10">
+					<div class="space-y-2 py-6">
+						<a
+							href="/"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							>Home</a
+						>
+						<a
+							href="/"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							>About</a
+						>
+						<a
+							href="/"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							>Team</a
+						>
+						<a
+							href="/"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							>Investment</a
+						>
+					</div>
+					<div class="py-6">
+						<a
+							href="/"
+							class="-mx-3 block rounded-lg px-3 py-2.5 text-base leading-7 hover:bg-[#ededed]"
+							>Apply for funding </a
+						>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Mobile menu ends here -->
+</header>
+
+<!-- Hero section starts here -->
+<section class="py-24">
+	<div class="container mx-auto">
+		<div class="grid grid-cols-2 gap-x-48">
+			<div class="space-y-6">
+				<h1 class="text-white text-6xl font-medium font-cg">A First of it’s Kind Celebrity led Global VC Fund</h1>
+				<p class="text-xl">Da One Global Ventures is a VC fund founded by international cricketing icon Shikhar Dhawan, committed  to investing in innovation across the wide spectrum of Sports.</p>
+				<button class="text-base leading-6 hover:font-semibold border border-[#0086e2] rounded-full px-8 py-2">Apply for funding <span aria-hidden="true">&rarr;</span></button>
+			</div>
+			<div>
+				<img src="./shikhar-dhawan-daone.jpeg" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Hero section ends here -->
+
+<!-- Investment section starts here -->
+<section class="py-24">
+	<div class="container mx-auto">
+		<div class="space-y-16">
+			<div class="flex flex-col items-center gap-4">
+				<h2 class="text-white text-5xl font-medium font-cg">Global Co-Investment Opportunities</h2>
+				<p class="text-xl">We have access to a large pool of quality deals, thanks to our extensive network and ability to collaborate.</p>
+			</div>
+			<div class="grid grid-cols-3 gap-8">
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Co-Investments & Partnerships</h3>
+					<p class="text-base">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Direct Sourcing</h3>
+					<p class="text-base">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Community Building</h3>
+					<p class="text-base">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Co-Investments & Partnerships</h3>
+					<p class="text-base">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Direct Sourcing</h3>
+					<p class="text-base">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Community Building</h3>
+					<p class="text-base">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Co-Investments & Partnerships</h3>
+					<p class="text-base">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Direct Sourcing</h3>
+					<p class="text-base">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
+				</div>
+				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+					<h3 class="text-white text-lg font-medium font-cg">Community Building</h3>
+					<p class="text-base">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Investment section ends here -->
+
+<!-- Team section starts here -->
+<section></section>
+<!-- Team section ends here -->
+
+<!-- FAQ section starts here -->
+<section></section>
+<!-- FAQ section ends here -->
+
+<!-- Apply form starts here -->
+<section></section>
+<!-- Apply form ends here -->
+
+<!-- Footer starts here -->
+<footer></footer>
+<!-- Footer ends here -->
