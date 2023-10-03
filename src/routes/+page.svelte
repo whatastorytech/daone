3a43137ae2649ce05c98d08eb1ea6d80a29e7360
Raunak Hajela <raunakhajela@gmail.com>
2023-10-03 10:34:10 +0000

    Add backdrop filter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,7 +36,7 @@
 			<a href="/" class="text-base leading-6 hover:underline">Investment</a>
 		</div>
 		<div class="hidden lg:flex lg:flex-1 lg:justify-end">
-			<a href="/" class="text-base leading-6 border border-[#0086e2] hover:bg-[#011523] rounded-full px-12 py-3">Apply for funding <span aria-hidden="true">&rarr;</span></a
+			<a href="/" class="text-white text-lg leading-6 border border-[#0086e2] bg-[#022842] hover:bg-[#021f32] rounded-full px-12 py-3">Apply for funding <span aria-hidden="true">&rarr;</span></a
 			>
 		</div>
 	</nav>
@@ -68,29 +68,29 @@
 					<div class="space-y-2 py-6">
 						<a
 							href="/"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-6 hover:bg-[#ededed]"
 							>Home</a
 						>
 						<a
 							href="/"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-6 hover:bg-[#ededed]"
 							>About</a
 						>
 						<a
 							href="/"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-6 hover:bg-[#ededed]"
 							>Team</a
 						>
 						<a
 							href="/"
-							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-7 hover:bg-[#ededed]"
+							class="-mx-3 block rounded-lg px-3 py-2 text-base leading-6 hover:bg-[#ededed]"
 							>Investment</a
 						>
 					</div>
 					<div class="py-6">
 						<a
 							href="/"
-							class="-mx-3 block rounded-lg px-3 py-2.5 text-base leading-7 hover:bg-[#ededed]"
+							class="-mx-3 block rounded-lg px-3 py-2.5 text-base leading-6 hover:bg-[#ededed]"
 							>Apply for funding </a
 						>
 					</div>
@@ -102,13 +102,13 @@
 </header>
 
 <!-- Hero section starts here -->
-<section class="py-24">
+<section class="py-16 md:py-24 lg:py-32">
 	<div class="container mx-auto">
 		<div class="grid grid-cols-2 gap-x-48">
 			<div class="space-y-6">
 				<h1 class="text-white text-6xl font-medium font-cg">A First of it’s Kind Celebrity led Global VC Fund</h1>
-				<p class="text-xl">Da One Global Ventures is a VC fund founded by international cricketing icon Shikhar Dhawan, committed  to investing in innovation across the wide spectrum of Sports.</p>
-				<button class="text-base leading-6 border border-[#0086e2] hover:bg-[#011523] rounded-full px-12 py-3">Apply for funding <span aria-hidden="true">&rarr;</span></button>
+				<p class="text-xl leading-8">Da One Global Ventures is a VC fund founded by international cricketing icon Shikhar Dhawan, committed  to investing in innovation across the wide spectrum of Sports.</p>
+				<button class="text-white text-lg leading-6 border border-[#0086e2] bg-[#022842] hover:bg-[#021f32] rounded-full px-12 py-3">Apply for funding <span aria-hidden="true">&rarr;</span></button>
 			</div>
 			<div>
 				<img src="./shikhar-dhawan-daone.jpeg" alt="">
@@ -119,8 +119,19 @@
 <!-- Hero section ends here -->
 
 <!-- Investment section starts here -->
-<section class="py-24">
-	<div class="container mx-auto">
+<section class="py-16 md:py-24 lg:py-32 border-y border-[#021f32]">
+	<!-- backdrop circle starts here -->
+	<div class="relative isolate">
+		<div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+			<div
+			class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#002cff] to-[#0086e2] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+			</div>
+		</div>
+	</div>
+	<!-- backdrop circle ends here -->
+	
+	  <div class="container mx-auto">
 		<div class="space-y-16">
 			<div class="flex flex-col items-center gap-4">
 				<h2 class="text-white text-5xl font-medium font-cg">Global Co-Investment Opportunities</h2>
@@ -129,39 +140,39 @@
 			<div class="grid grid-cols-3 gap-8 px-32">
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Co-Investments & Partnerships</h3>
-					<p class="text-base">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
+					<p class="text-base leading-6">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Direct Sourcing</h3>
-					<p class="text-base">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
+					<p class="text-base leading-6">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Community Building</h3>
-					<p class="text-base">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
+					<p class="text-base leading-6">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Co-Investments & Partnerships</h3>
-					<p class="text-base">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
+					<p class="text-base leading-6">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Direct Sourcing</h3>
-					<p class="text-base">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
+					<p class="text-base leading-6">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Community Building</h3>
-					<p class="text-base">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
+					<p class="text-base leading-6">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Co-Investments & Partnerships</h3>
-					<p class="text-base">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
+					<p class="text-base leading-6">Capitalizing on our vast global network, we foresee numerous co-investment prospects and exclusive sports industry partnerships.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Direct Sourcing</h3>
-					<p class="text-base">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
+					<p class="text-base leading-6">By actively tracking SportsTech developments and monitoring leading entrepreneurs, we strategically approach them for investment at opportune moments.</p>
 				</div>
 				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
 					<h3 class="text-white text-lg font-medium font-cg">Community Building</h3>
-					<p class="text-base">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
+					<p class="text-base leading-6">Focus on building a strong community comprising investment bankers, advisors, consultants, angel investors, serial entrepreneurs within the sports tech domain.</p>
 				</div>
 			</div>
 		</div>
@@ -170,7 +181,7 @@
 <!-- Investment section ends here -->
 
 <!-- Team section starts here -->
-<section class="py-24">
+<section class="py-16 md:py-24 lg:py-32 border-y border-[#021f32]">
 	<div class="container mx-auto">
 		<div class="space-y-16">
 			<div class="flex flex-col items-center gap-4">
@@ -178,50 +189,72 @@
 				<p class="text-xl">We have access to a large pool of quality deals, thanks to our extensive network and ability to collaborate.</p>
 			</div>
 			<div class="grid grid-cols-4 gap-8 px-32">
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a1.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Andrew Bell</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a2.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Dr. Sandeep Gandhi</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a3.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Amitesh Shah</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a4.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Abhinav Tandon</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a5.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Akshay Chhugani</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a6.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Maharshi Ganatra</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a1.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Kumar Gaurav</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a3.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Akshay Grover</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a5.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Geetansh Sahni</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a2.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Rajeev Duggal</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a4.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Abhinav Tandon</h3>
 				</div>
-				<div class="space-y-3 border border-blue-50 border-opacity-30 rounded-md p-8">
+				<div class="space-y-3">
+					<img src="./a6.jpg" alt="">
 					<h3 class="text-white text-lg font-medium font-cg">Raunak Hajela</h3>
 				</div>
 			</div>
 		</div>
 	</div>
+	<!-- backdrop circle starts here -->
+	<div class="relative isolate">
+		<div class="absolute inset-x-0 -top-10 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" aria-hidden="true">
+			<div
+			class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#002cff] to-[#0086e2] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+			style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
+			</div>
+		</div>
+	</div>
+	<!-- backdrop circle ends here -->
 </section>
 <!-- Team section ends here -->
 
 <!-- FAQ section starts here -->
-<section></section>
+<section class="py-16 md:py-24 lg:py-32 border-y border-[#021f32]"></section>
 <!-- FAQ section ends here -->
 
 <!-- Apply form starts here -->
