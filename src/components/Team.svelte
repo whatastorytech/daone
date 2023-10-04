32bcaf5c1b34cb6ba3a9378284a4e6ac096ae971
Manjunath G <manjumjng2@gmail.com>
2023-10-04 11:13:17 +0530

    Updated Gradients style and created divider component

--- src/components/Team.svelte
+++ src/components/Team.svelte
@@ -1,6 +1,23 @@
+<script>
+	import GradientDivider from '../components/GradientDivider.svelte';
+</script>
 <!-- Team section starts here -->
-<section class="py-16 md:py-24 lg:py-32 border-y border-[#021f32]">
-	<div class="container mx-auto">
+<section class="py-16 md:py-24 lg:py-32 relative overflow-hidden">
+	<GradientDivider/>
+	<!-- backdrop circle starts here -->
+	<div class="absolute top-1/2 right-0 w-full isolate">
+		<div
+			class="absolute inset-x-0 -top-10 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+			aria-hidden="true"
+		>
+			<div
+				class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#002cff] to-[#0086e2] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+			></div>
+		</div>
+	</div>
+	<!-- backdrop circle ends here -->
+	<div class="container mx-auto relative z-10">
 		<div class="space-y-16">
 			<div class="flex flex-col items-center gap-4">
 				<h2 class="text-white text-5xl font-medium font-cg">Our Core Team</h2>
@@ -61,18 +78,6 @@
 			</div>
 		</div>
 	</div>
-	<!-- backdrop circle starts here -->
-	<div class="relative isolate">
-		<div
-			class="absolute inset-x-0 -top-10 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-			aria-hidden="true"
-		>
-			<div
-				class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#002cff] to-[#0086e2] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-				style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-			></div>
-		</div>
-	</div>
-	<!-- backdrop circle ends here -->
+
 </section>
 <!-- Team section ends here -->
