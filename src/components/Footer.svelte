8491c2d759cd18c23bec66f67f5cfe67faa0dcce
Raunak Hajela <raunakhajela@gmail.com>
2023-10-06 09:36:34 +0000

    Add stats and footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -2,17 +2,16 @@
 	import GradientDivider from '../components/GradientDivider.svelte';
 </script>
 <!-- Footer starts here -->
-<section class="py-16 md:py-24 lg:py-32 relative overflow-hidden">
+<section class="pt-16 pb-8 md:pt-24 lg:pt-32 relative overflow-hidden">
 	<GradientDivider />
 	<div class="container mx-auto">
-		<div class="space-y-16">
-			<div class="flex flex-col mx-auto max-w-3xl items-center justify-center gap-4">
-				<h3 class="text-white text-center text-5xl font-medium font-cg tracking-wide">Join us in shaping the future of sports with quality deals, funding and support.</h3>
-                <button
-					class="mt-10 text-white text-lg leading-6 border border-[#0086e2] bg-[#022842] hover:bg-[#021f32] rounded-full px-12 py-3"
-					>Pitch Us <span aria-hidden="true">&rarr;</span>
-                </button>
-			</div>
+		<div class="flex flex-col mx-auto max-w-3xl items-center justify-center gap-4">
+			<h3 class="text-white text-center text-5xl font-medium font-cg tracking-wide">Join us in shaping the future of sports with quality deals, funding and support.</h3>
+			<button
+				class="mt-10 text-white text-lg leading-6 border border-[#0086e2] bg-[#022842] hover:bg-[#021f32] rounded-full px-12 py-3"
+				>Pitch Us <span aria-hidden="true">&rarr;</span>
+			</button>
+			<p class="pt-20">© 2023 Da One Global Venutres</p>
 		</div>
 	</div>
 </section>
--- src/components/Team.svelte
+++ src/components/Team.svelte
@@ -123,6 +123,23 @@
 				</SplideTrack>
 			</div>
 		</Splide>
+		<div class="container mx-auto flex text-center justify-center gap-x-32 py-16">
+			<div class="space-y-2">
+				<div class="text-xl">Over</div>
+				<div class="text-white text-6xl tracking-wider uppercase font-bold font-cg">1,000</div>
+				<div class="text-xl">Industry leaders</div>
+			</div>
+			<div class="space-y-2">
+				<div class="text-xl">Immersive</div>
+				<div class="text-white text-6xl tracking-wider uppercase font-bold font-cg">Global</div>
+				<div class="text-xl">Sports network</div>
+			</div>
+			<div class="space-y-2">
+				<div class="text-xl">Over</div>
+				<div class="text-white text-6xl tracking-wider uppercase font-bold font-cg">80M</div>
+				<div class="text-xl">Funded startups</div>
+			</div>
+		</div>
 	</div>
 
 </section>
